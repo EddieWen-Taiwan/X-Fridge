e821f5240a7d03e49368098bd3f0af11d4a394c8
yac516joe <yac516joe@gmail.com>
2015-01-31 12:04:47 +0800

    add tool.js

--- Firefox/lib/main.js
+++ Firefox/lib/main.js
@@ -18,10 +18,16 @@ var button = buttons.ActionButton({
 });
 tabs.on('open', function onOpen(tab) {
 	tab.url = self.data.url("newtab.html");
+	// tabs.on('open');
+	// var newTabBrowser = gBrowser.getBrowserForTab(gBrowser.addTab("http://www.google.com/"));
+	// newTabBrowser.addEventListener("load", function () {
+	//   newTabBrowser.contentDocument.body.innerHTML = "<div>hello world</div>";
+	// }, true);
 });
-//tabs.on('activate', function (tab) {
-//	tab.url = self.data.url("newtab.html");
-//});
+tabs.on('activate', function(tab) {
+	var data = require("sdk/self").data;
+	var pageMod = require("sdk/page-mod");
+});
 
 function handleClick(state) {
 	//	tabs.open("./newtab.html");
