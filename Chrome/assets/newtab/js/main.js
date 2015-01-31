e9c58592e19ac9a51ae01f55df7d22b2a0bcef20
EddieWen-Taiwan <m20021991y@gmail.com>
2015-01-31 13:44:33 +0800

    Detect whether user is the first time

--- Chrome/assets/newtab/js/main.js
+++ Chrome/assets/newtab/js/main.js
@@ -1,17 +1,11 @@
 $(function () {
 	start();
 
-	$('#askOverlay').keypress( function(e){
-		if( e.keyCode == 13 ) {
-			var name = $('#askOverlay input').val();
-			tool.write( "username", name );
-			INFO.updateName();
-		}
-	});
-
 	function start() {
 
-		askUserName();
+		if( IsThisFirstTime() ) {
+			askUserName();
+		}
 
 		window.INFO = new INFO();
 		window.LIST = new LIST();
@@ -19,7 +13,21 @@ $(function () {
 	}
 
 	function askUserName() {
+		// show the question to ask the username
 		$('#askOverlay').removeClass('hidden');
+
+		// if ENTER be pressed 
+		$('#askOverlay').keypress( function(e){
+			if( e.keyCode == 13 ) {
+				var name = $('#askOverlay input').val();
+				tool.write( "username", name );
+				INFO.updateName();
+			}
+		});
+	}
+
+	function IsThisFirstTime() {
+		return tool.read( "username" ) == "" ? true : false;
 	}
 
 });
