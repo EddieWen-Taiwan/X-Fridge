029ff4e5ca2c2329b732998bb420a81f5b6a8134
Bonerchick <jerry82422@gmail.com>
2015-01-31 13:03:01 +0800

    fixed Date stringFormat

--- Chrome/assets/newtab/app.js
+++ Chrome/assets/newtab/app.js
@@ -2,7 +2,8 @@ Date.prototype.stringFormat = function () {
 	var y = this.getFullYear();
 	var m = this.getMonth() + 1;
 	var d = this.getDate();
-	return y + "-" + m + "-" + "d";
+	var pad = '00';
+	return y + "-" + (pad + m).slice(-pad.length) + "-" + (pad + d).slice(-pad.length);
 };
 function INFO() {
 	var self = this;
--- Chrome/assets/newtab/js/extends.js
+++ Chrome/assets/newtab/js/extends.js
@@ -2,5 +2,6 @@ Date.prototype.stringFormat = function () {
 	var y = this.getFullYear();
 	var m = this.getMonth() + 1;
 	var d = this.getDate();
-	return y + "-" + m + "-" + "d";
+	var pad = '00';
+	return y + "-" + (pad + m).slice(-pad.length) + "-" + (pad + d).slice(-pad.length);
 };
