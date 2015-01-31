7218bcd81f5742d4a34e43a30389f5f7f8764927
EddieWen-Taiwan <m20021991y@gmail.com>
2015-01-31 12:35:44 +0800

    Add a div called askOverlay

--- Chrome/assets/newtab/js/main.js
+++ Chrome/assets/newtab/js/main.js
@@ -2,8 +2,16 @@ $(function () {
 	start();
 
 	function start() {
+
+		askUserName();
+
 		window.INFO = new INFO();
 		window.LIST = new LIST();
 		INFO.updateName().updateDate();
 	}
-})
+
+	function askUserName() {
+		
+	}
+
+});
